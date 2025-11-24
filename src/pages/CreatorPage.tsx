43e5c6727d18418f932d7db722528887dbd4b1f2
Chin Chee Seng <cheeseng.chin@sicmsb.com>
2025-11-24 17:40:56 +0800

    third commit

--- src/pages/CreatorPage.tsx
+++ src/pages/CreatorPage.tsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 import { SurveyCreatorComponent, SurveyCreator } from "survey-creator-react";
 import "survey-core/survey-core.min.css";
 import "survey-creator-core/survey-creator-core.min.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setSurveyJson } from "../store/surveySlice";
 import SurveyCreatorTheme from "survey-creator-core/themes";
 import { registerCreatorTheme, registerSurveyTheme } from "survey-creator-core";
 import SurveyTheme from "survey-core/themes";
+// ✅ Fixed Import
+import type { RootState } from "../store/store";
 
 const creatorOptions = {
-  showLogicTab: false,
+  showLogicTab: true,
   isAutoSave: false,
   showThemeTab: true,
   showJsonEditorTab: false,
@@ -17,23 +19,31 @@ const creatorOptions = {
 
 const CreatorPage: React.FC = () => {
   const dispatch = useDispatch();
+  const savedSurveyJson = useSelector(
+    (state: RootState) => state.survey.surveyJson
+  );
+
   const [creator, setCreator] = useState<SurveyCreator | null>(null);
 
   useEffect(() => {
     const newCreator = new SurveyCreator(creatorOptions);
     newCreator.applyCreatorTheme(SurveyCreatorTheme.DefaultContrast);
-
     registerCreatorTheme(SurveyCreatorTheme);
     registerSurveyTheme(SurveyTheme);
+
+    if (savedSurveyJson) {
+      newCreator.JSON = savedSurveyJson;
+    }
+
     newCreator.saveSurveyFunc = (
       saveNo: number,
       callback: (no: number, isSuccess: boolean) => void
     ) => {
-      // Save to Redux
       console.log("Saving survey...", newCreator.JSON);
       dispatch(setSurveyJson(newCreator.JSON));
       callback(saveNo, true);
     };
+
     setCreator(newCreator);
   }, [dispatch]);
 
